7e96a2f8bbeb9701539b710c3210820aab70f0dc
Zied <benmlahzied@gmail.com>
2021-10-10 18:16:30 +0200

    Added equals hashcode to Operation

--- src/main/java/com/zbm/kata/bank/domain/Operation.java
+++ src/main/java/com/zbm/kata/bank/domain/Operation.java
@@ -2,6 +2,7 @@ package com.zbm.kata.bank.domain;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.StringJoiner;
 
 public class Operation {
@@ -16,6 +17,19 @@ public class Operation {
         this.type = type;
         this.currentBalance = currentBalance;
         this.dateTime = dateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operation operation = (Operation) o;
+        return type == operation.type && dateTime.equals(operation.dateTime) && amount.equals(operation.amount) && currentBalance.equals(operation.currentBalance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, dateTime, amount, currentBalance);
     }
 
     @Override
--- src/test/java/com/zbm/kata/bank/service/OperationTest.java
+++ src/test/java/com/zbm/kata/bank/service/OperationTest.java
@@ -3,13 +3,33 @@ package com.zbm.kata.bank.service;
 import com.zbm.kata.bank.domain.Operation;
 import com.zbm.kata.bank.domain.OperationType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OperationTest {
+
+    @ParameterizedTest
+    @MethodSource("createEqualsTestCases")
+    void shouldEqualsReturnExpectedBoolean (Operation operation1, Operation operation2, boolean expectedResult) {
+        assertThat(operation1.equals(operation2)).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldEqualsBeSymmetric() {
+        LocalDateTime now = LocalDateTime.now();
+        Operation operation1 = new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN);
+        Operation operation2 = new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN);
+        assertThat(operation1.equals(operation2)).isTrue();
+        assertThat(operation2.equals(operation1)).isTrue();
+        assertThat(operation1).hasSameHashCodeAs(operation2.hashCode());
+    }
 
     @Test
     void shouldReturnOperationProperties() {
@@ -19,5 +39,21 @@ class OperationTest {
         assertThat(operation.getAmount()).isEqualTo(BigDecimal.TEN);
         assertThat(operation.getCurrentBalance()).isEqualTo(BigDecimal.TEN);
         assertThat(operation.getDateTime()).isEqualTo(now);
+    }
+
+    private static Stream<Arguments> createEqualsTestCases() {
+        LocalDateTime now = LocalDateTime.now();
+        return Stream.of(
+                Arguments.of(new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN),
+                        new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN), true),
+                Arguments.of(new Operation(now.plusHours(1), BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN),
+                        new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN), false),
+                Arguments.of(new Operation(now, BigDecimal.ZERO, OperationType.DEPOSIT, BigDecimal.TEN),
+                        new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN), false),
+                Arguments.of(new Operation(now, BigDecimal.TEN, OperationType.WITHDRAW, BigDecimal.TEN),
+                        new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN), false),
+                Arguments.of(new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.ZERO),
+                        new Operation(now, BigDecimal.TEN, OperationType.DEPOSIT, BigDecimal.TEN), false)
+        );
     }
 }
