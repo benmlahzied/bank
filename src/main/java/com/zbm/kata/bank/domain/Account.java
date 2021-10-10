8d4f5987f49749a0861bbaa3d7f0649bcc275bec
Zied <benmlahzied@gmail.com>
2021-10-10 17:12:22 +0200

    [US 1] Implemented deposit rules

--- src/main/java/com/zbm/kata/bank/domain/Account.java
+++ src/main/java/com/zbm/kata/bank/domain/Account.java
@@ -4,11 +4,20 @@ import java.math.BigDecimal;
 
 public class Account {
 
-    public BigDecimal getTotalBalance() {
-        return null;
+    private BigDecimal balance;
+
+    public Account() {
+        balance = BigDecimal.ZERO;
     }
 
     public void deposit(BigDecimal amount) {
+        if (amount.signum() <= 0) {
+            throw new IllegalArgumentException("Amount should be greater than zero.");
+        }
+        balance = balance.add(amount);
+    }
 
+    public BigDecimal getTotalBalance() {
+        return this.balance;
     }
 }
