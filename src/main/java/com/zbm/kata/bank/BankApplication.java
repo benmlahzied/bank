83e2bc20ba2bc5b9c17a5ef60c77841bcf28d393
Zied <benmlahzied@gmail.com>
2021-10-10 17:50:54 +0200

    Added main scenario in main Class

--- src/main/java/com/zbm/kata/bank/BankApplication.java
+++ src/main/java/com/zbm/kata/bank/BankApplication.java
@@ -1,9 +1,20 @@
 package com.zbm.kata.bank;
 
+import com.zbm.kata.bank.domain.Account;
+import com.zbm.kata.bank.service.ConsoleStatementPrintingService;
+import com.zbm.kata.bank.service.StatementPrintingService;
+
+import java.math.BigDecimal;
+
 public class BankApplication {
 
     public static void main(String... args) {
-        throw new UnsupportedOperationException();
+        StatementPrintingService statementPrintingService = new ConsoleStatementPrintingService();
+        Account account = new Account(statementPrintingService);
+        account.deposit(BigDecimal.valueOf(1000));
+        account.deposit(BigDecimal.TEN);
+        account.withdraw(BigDecimal.valueOf(100));
+        account.printStatement();
     }
 }
 
